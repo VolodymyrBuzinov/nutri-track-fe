6d53f30c0bb23a37d8073d20c193e5c4bad23094
VolodymyrBuzinov <vova.buzz@gmail.com>
2026-07-21 12:04:51 +0300

    add basic content colors

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/80",
+        default:
+          "border-main bg-main text-primary-foreground hover:bg-background hover:text-main",
         outline:
           "border-border hover:bg-input/50 hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-input/30",
         secondary:
