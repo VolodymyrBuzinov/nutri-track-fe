3324e65912328bcd762675f7b08509757897e6e4
VolodymyrBuzinov <vova.buzz@gmail.com>
2026-07-29 11:39:28 +0300

    add toast component and connect its provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AppErrorBoundary } from "@/components/custom/shared/AppErrorBoundary";
 import { routesData } from "@/routing/routesData";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastProvider } from "@/components/ui/toast";
 
 function App() {
   return (
     <AppErrorBoundary>
+      <ToastProvider />
       <BrowserRouter>
         <Routes>
           {routesData.map((route) => (
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { toast } from "@/components/ui/toast";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+type ApiError = {
+  data: {
+    message: string;
+    code: string;
+  };
+};
+export const handleApiError = (error: ApiError) => {
+  toast.add({
+    title: "Помилка",
+    description: error.data.message,
+    type: "error",
+  });
+};
